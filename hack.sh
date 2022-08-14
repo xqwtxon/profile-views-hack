d2b5bc457c50a72e5ee1fa723ee09971d721b2de
Ace Sobremont <xqwtxon@hotmail.com>
2022-08-14 15:20:33 +0800

    Ez

---
+++ hack.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+while true; do curl -A "github-camo" -s https://komarev.com/ghpvc/?username=xqwtxon&style=for-the-badge&color=brightgreen&label=Profile+Views; done
+while true; do curl -A "github-camo" -s https://komarev.com/ghpvc/?username=xqwtxon&style=for-the-badge&color=brightgreen&label=Profile+Views; done
+while true; do curl -A "github-camo" -s https://komarev.com/ghpvc/?username=xqwtxon&style=for-the-badge&color=brightgreen&label=Profile+Views; done
+while true; do curl -A "github-camo" -s https://komarev.com/ghpvc/?username=xqwtxon&style=for-the-badge&color=brightgreen&label=Profile+Views; done
