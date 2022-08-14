53556bd97580884db6f991b594c762ddb85e76c0
xqwtxon <xqwtxon@hotmail.com>
2022-08-14 20:27:59 +0800

    Added Useragent feature

--- hack.sh
+++ hack.sh
@@ -13,7 +13,7 @@ count="0"
 while true
 	do
 	echo -n "[*] Adding Profile Views: "
-	curl -sL -A "github-camo" $(cat config/url.txt) >> "views.cache"
+	curl -sL -A "$(cat config/useragent.txt)" $(cat config/url.txt) >> "views.cache"
 	if [ ! "$?" == "0" ]; then
 	echo "~"
 	else count="$(expr ${count} + 1)" && echo "${count}" && rm "views.cache"
