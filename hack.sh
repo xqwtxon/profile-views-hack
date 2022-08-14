33e705c8cf1ab4512f69f9da17db941234ac8837
Ace Sobremont <xqwtxon@hotmail.com>
2022-08-14 17:53:33 +0800

    dest

--- hack.sh
+++ hack.sh
@@ -7,7 +7,7 @@ fi
 while true
 	do
 	echo -n "[*] Adding Profile Views: "
-	curl -sL -A "github-camo" https://github.com/${username} >> "views.cache"
+	curl -sL -A "github-camo" $(cat config/url.txt) >> "views.cache"
 	if [ ! "$?" == "0" ]; then
 	echo "~"
 	else count="$(expr ${count} + 1)" && echo "${count}" && rm "views.cache"
