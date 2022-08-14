29735e425cfac983f45b0d3fae04980691cc3618
junjun2527 <xqwtxon@hotmail.com>
2022-08-14 17:45:26 +0800

    Improved

--- hack.sh
+++ hack.sh
@@ -1,3 +1,15 @@
-#!/bin/bash
-username="$(cat username.txt)"
-while sleep 1s; do curl -sL -A "github-camo" https://komarev.com/ghpvc/?username=${username}&style=for-the-badge&color=brightgreen&label=Profile+Views; done
+username="$(cat config/username.txt)"
+count="0"
+if [ "$(connect -n https://github.com/)" == "1" ]; then
+	echo "[!] No Internet Connection!" && exit 1
+else echo "[!] Starting Profile Views Hack by @xqwtxon" && sleep 5s
+fi
+while true
+	do
+	echo -n "[*] Adding Profile Views: "
+	curl -sL -A "github-camo" https://github.com/${username} >> "views.cache"
+	if [ ! "$?" == "0" ]; then
+	echo "~"
+	else count="$(expr ${count} + 1)" && echo "${count}" && rm "views.cache"
+	fi
+done
