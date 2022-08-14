d848d15895419e5b2a8cbd6155420c53245f4eaa
junjun2527 <xqwtxon@hotmail.com>
2022-08-14 19:37:20 +0800

    Useless

--- hack.sh
+++ hack.sh
@@ -1,8 +1,4 @@
 count="0"
-if [ "$(connect -n https://github.com/)" == "1" ]; then
-	echo "[!] No Internet Connection!" && exit 1
-else echo "[!] Starting Profile Views Hack by @xqwtxon" && sleep 5s
-fi
 while true
 	do
 	echo -n "[*] Adding Profile Views: "
