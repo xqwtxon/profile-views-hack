7cbe34f0e01a0f9bb35ce3173b0d33589267c628
Ace Sobremont <xqwtxon@hotmail.com>
2022-08-14 15:28:57 +0800

    Fix

--- hack.sh
+++ hack.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-while sleep 2s; do curl -A "github-camo" -s https://komarev.com/ghpvc/?username=xqwtxon&style=for-the-badge&color=brightgreen&label=Profile+Views; done
+while sleep 1s; do curl -sL -A "github-camo" https://komarev.com/ghpvc/?username=xqwtxon&style=for-the-badge&color=brightgreen&label=Profile+Views; done
